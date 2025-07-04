a62e5dcd02cd1e7e0bc04c14098474ebc6daaba6
KarolSalvador <karolsalvador91@gmail.com>
2025-07-04 08:11:58 -0300

    exclusao de components que nao serao utilizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
-import { MenuTitleComponent } from './components/menu-title/menu-title.component';
-import { BigCardComponent } from './components/big-card/big-card.component';
-import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SobreComponent } from './components/sobre/sobre.component';
 import { ProjetosComponent } from './components/projetos/projetos.component';
@@ -15,9 +12,6 @@ import { ProjetosComponent } from './components/projetos/projetos.component';
   declarations: [
     AppComponent,
     MenuBarComponent,
-    MenuTitleComponent,
-    BigCardComponent,
-    SmallCardComponent,
     HomeComponent,
     SobreComponent,
     ProjetosComponent,
